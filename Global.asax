b54cb059ccdf28df59dbd31916edb4e8eba3687e
cfirgens <firgenscj@gmail.com>
2019-08-02 10:23:47 -0500

    init commit; created superhero model

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Superhero_Project.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Superhero_Project.Startup))]
+namespace Superhero_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
